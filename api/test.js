22488801b0820b2a33cca7e47a81a0ab222539c1
apanupam <aproy48@gmail.com>
2026-03-04 16:39:52 +0530

    Expose DB URL prefix to debug

--- api/test.js
+++ api/test.js
@@ -7,6 +7,7 @@ export default function handler(req, res) {
         hasDbUrl: !!process.env.DATABASE_URL,
         hasPostgresUrl: !!process.env.POSTGRES_URL,
         dbDialect: sequelize.getDialect(),
-        dbInitError: sequelize._initError || null
+        dbInitError: sequelize._initError || null,
+        dbUrlPrefix: process.env.DATABASE_URL ? process.env.DATABASE_URL.substring(0, 15) : "none"
     });
 }
